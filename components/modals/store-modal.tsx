a79f60f1f4bd973b31c98e6fb0ea2e2b683c8ee3
uhassantoor <uhassantoor@gmail.com>
2024-11-07 18:25:35 +0500

    Form

--- components/modals/store-modal.tsx
+++ components/modals/store-modal.tsx
@@ -1,11 +1,28 @@
 "use client";
+import * as z from "zod";
 
 import {useStoreModalStore} from '@/hooks/use-store-modal';
 import {Modal} from '@/components/ui/modal'
+import {useForm} from 'react-hook-form'
+import {zodResolver} from '@hookform/resolvers/zod'
+import {Form} from "@/components/ui/form"
+
+const formSchema = z.object({
+    name: z.string().min(1),
+})
 
 export const StoreModal = () => {
     const storeModal = useStoreModalStore();
 
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema), 
+        defaultValues: {
+            name: "", 
+        }
+    })
+    const onSubmit = async (values: z.infer<typeof formSchema>) => {
+        console.log(values)
+    }
     return (
         <Modal
         title='Create Store'
@@ -13,7 +30,16 @@ export const StoreModal = () => {
         isOpen={storeModal.isOpen}
         onClose={storeModal.onClose}
     >
-        Future Create Store Form
+       <div>
+         <div className="space-y-4 py-2 pb-4">
+           <Form {...form}>
+            <form>
+
+            </form>
+
+           </Form>
+         </div>
+       </div>
     </Modal>
     )
 }
---
+++ components/ui/form.tsx
@@ -0,0 +1,12 @@
+// form.js or form.tsx
+import React from 'react';
+
+const Form = () => {
+  return (
+    <form>
+      {/* form elements */}
+    </form>
+  );
+};
+
+export default Form; // or export const Form = () => { ... }
